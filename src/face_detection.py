2a96f9c69529acf95240454ca99f7d2cc4d6bd17
Lucas Vandroux <lucas.vandroux@gmail.com>
2020-06-19 20:46:59 +0200

    Add preprocess and postprocess functions

--- src/face_detection.py
+++ src/face_detection.py
@@ -1,7 +1,12 @@
-'''
-This is a sample class for a model. You may choose to use it as-is or make any changes to it.
-This has been provided just to give you an idea of how to structure your model class.
-'''
+import os
+import sys
+import time
+
+import cv2
+import numpy as np
+
+from openvino.inference_engine import IENetwork, IECore
+
 
 class FaceDetectionModel:
     '''
@@ -29,11 +34,14 @@ class FaceDetectionModel:
         self.model_weights_path = os.path.join(self.model_dir_path, self.model_name + '.bin')
         
         self.device = device
-        self.extensions_path = extension
+        self.extensions_path = extensions_path
 
     def load_model(self):
         """ Load the model 
-        """        
+        """
+        # Initialize timer
+        start_time = time.time()
+
         # Initialize the plugin
         self.plugin = IECore()
 
@@ -43,13 +51,13 @@ class FaceDetectionModel:
         # Load the extensions that might be needed
         # No need of CPU extension with the 2020.3 version of OpenVINO
         if self.extensions_path is not None:
-            plugin.add_extension(extension_path=self.extensions_path, device_name=self.device)
+            self.plugin.add_extension(extension_path=self.extensions_path, device_name=self.device)
         
         # Check for supported layers
-        supported_layers = self.plugin.query_network(network=self.network, device_name=device)
+        supported_layers = self.plugin.query_network(network=self.network, device_name=self.device)
         unsupported_layers = [l for l in self.network.layers.keys() if l not in supported_layers]
         if len(unsupported_layers) != 0:
-            sys.exit(f"[ ERR ] Unsupported layers found in model {self.model_name}: {unsupported_layers}.")
+            sys.exit(f"[ ERRO ] Unsupported layers found in model {self.model_name}: {unsupported_layers}.")
 
         # Load the IENetwork into the plugin
         self.exec_network = self.plugin.load_network(self.network, self.device)
@@ -57,6 +65,8 @@ class FaceDetectionModel:
         # Get the input layer and output layer
         self.input_blob = next(iter(self.network.inputs))
         self.output_blob = next(iter(self.network.outputs))
+
+        print(f"[ INFO ] {self.model_name} loaded in {time.time() - start_time}s.")
 
     def get_input_shape(self):
         """ Gets the input shape of the network
@@ -74,7 +84,7 @@ class FaceDetectionModel:
         results (): 
         '''
         # Pre process the image
-        input_image = preprocess_input(image)
+        input_image = self.preprocess_input(image)
 
         # Create the input dictionary
         input_dict = {self.input_blob: input_image}
@@ -83,23 +93,60 @@ class FaceDetectionModel:
         output = self.exec_network.infer(input_dict)
 
         # Post process the output
-        results = postprocess_output(output)
+        results = self.postprocess_output(output)
 
         return results
 
-    def check_model(self):
-        raise NotImplementedError
-
     def preprocess_input(self, image):
-    '''
-    Before feeding the data into the model for inference,
-    you might have to preprocess it. This function is where you can do that.
-    '''
-        raise NotImplementedError
+        '''
+        Preprocess the image
+
+        Args:
+        image (numpy.array BGR): image to preprocess
+
+        Returns:
+        input_image (numpy.array): array containing the image with the dimensions [1x3x384x672]
+        '''
+        # Get the input shape of the model
+        net_input_shape = self.get_input_shape()
+
+        # Resize the image to the input of the model
+        input_image = cv2.resize(image, (net_input_shape[3], net_input_shape[2]))
+
+        # Get the Color Channels in front
+        input_image = input_image.transpose((2,0,1))
+
+        # Add one dimension at the front for the batch size
+        input_image = input_image.reshape(1, *input_image.shape)
+
+        return input_image
 
     def postprocess_output(self, outputs):
-    '''
-    Before feeding the output of this model to the next model,
-    you might have to preprocess the output. This function is where you can do that.
-    '''
-        raise NotImplementedError
+        '''
+        Postprocss the output of the model
+
+        Args:
+        outputs (numpy.array): array of the dimensions [1, 1, #detections, 7]
+
+        Returns:
+        results (list):
+        '''
+        num_detections = outputs.shape[2]
+        results = []
+
+        for idx in range(num_detections):
+            detection = outputs[1,1,idx,:]
+            results.append({
+                'label': detection[1],
+                'conf': detection[2],
+                'x_min': detection[3],
+                'y_min': detection[4],
+                'x_max': detection[5],
+                'y_max': detection[6]
+            })
+
+        # Order results with biggest conf first
+        sorted(results, key = lambda i: i['conf'], reverse=True)
+
+        return results
+
