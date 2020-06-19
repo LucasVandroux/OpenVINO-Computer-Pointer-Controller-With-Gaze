c5af2e582cb00b05748e3bc1e3baea3e18884524
Lucas Vandroux <lucas.vandroux@gmail.com>
2020-06-19 22:07:35 +0200

    Parent class for all OpenVINO models

--- src/model.py
+++ src/model.py
@@ -1,46 +1,133 @@
-'''
-This is a sample class for a model. You may choose to use it as-is or make any changes to it.
-This has been provided just to give you an idea of how to structure your model class.
-'''
+import os
+import sys
+import time
 
-class Model_X:
+import cv2
+import numpy as np
+
+from openvino.inference_engine import IENetwork, IECore
+
+class OpenVINOModel:
     '''
-    Class for the Face Detection Model.
+    Parent Class for the OpenVino Models
     '''
-    def __init__(self, model_name, device='CPU', extensions=None):
+    def __init__(self, model_xml_path, device='CPU', extensions_path=None):
         '''
-        TODO: Use this to set your instance variables.
+        Initialize the model
+
+        Args:
+        model_xml_path (str): path to the model's structure in same folder as the .bin file
+        device (str: 'CPU'): device to load the model on
+        extensions_path (str | None: None): extensions to load in case it is needed
         '''
-        raise NotImplementedError
+        # Extract the path to the directory containing the models files
+        self.model_dir_path, filename = os.path.split(model_xml_path)
+        
+        # Extract the name of the model
+        self.model_name = filename.split('.')[0]
+        
+        # Create the path to the .xml file describing the structure of the model
+        self.model_structure_path = os.path.join(self.model_dir_path, self.model_name + '.xml')
+       
+        # Create the path to the .bin file containing the weights of the model
+        self.model_weights_path = os.path.join(self.model_dir_path, self.model_name + '.bin')
+        
+        self.device = device
+        self.extensions_path = extensions_path
 
     def load_model(self):
-        '''
-        TODO: You will need to complete this method.
-        This method is for loading the model to the device specified by the user.
-        If your model requires any Plugins, this is where you can load them.
-        '''
-        raise NotImplementedError
+        """ Load the model 
+        """
+        # Initialize timer
+        start_time = time.time()
 
-    def predict(self, image):
-        '''
-        TODO: You will need to complete this method.
-        This method is meant for running predictions on the input image.
-        '''
-        raise NotImplementedError
+        # Initialize the plugin
+        self.plugin = IECore()
 
-    def check_model(self):
-        raise NotImplementedError
+        # Read IR as a IENetwork
+        self.network = IENetwork(model=self.model_structure_path, weights=self.model_weights_path)
+
+        # Load the extensions that might be needed
+        # No need of CPU extension with the 2020.3 version of OpenVINO
+        if self.extensions_path is not None:
+            self.plugin.add_extension(extension_path=self.extensions_path, device_name=self.device)
+        
+        # Check for supported layers
+        supported_layers = self.plugin.query_network(network=self.network, device_name=self.device)
+        unsupported_layers = [l for l in self.network.layers.keys() if l not in supported_layers]
+        if len(unsupported_layers) != 0:
+            sys.exit(f"[ ERRO ] Unsupported layers found in model {self.model_name}: {unsupported_layers}.")
+
+        # Load the IENetwork into the plugin
+        self.exec_network = self.plugin.load_network(self.network, self.device)
+
+        # Get the input layer and output layer
+        self.input_blob = next(iter(self.network.inputs))
+        self.output_blob = next(iter(self.network.outputs))
+
+        print(f"[ INFO ] {self.model_name} loaded in {time.time() - start_time}s.")
+
+    def get_input_shape(self):
+        """ Gets the input shape of the network
+        """
+        return self.network.inputs[self.input_blob].shape
+
+    def inference(self, image):
+        '''
+        Do the inference on an image
+        
+        Args:
+        image (numpy.array BGR): image to do the inference on
+
+        Returns:
+        output : return the output of the model 
+        '''
+        # Pre process the image
+        input_image = self.preprocess_input(image)
+
+        # Create the input dictionary
+        input_dict = {self.input_blob: input_image}
+
+        # Do the inference
+        output = self.exec_network.infer(input_dict)
+
+        return output
 
     def preprocess_input(self, image):
-    '''
-    Before feeding the data into the model for inference,
-    you might have to preprocess it. This function is where you can do that.
-    '''
+        '''
+        Preprocess the image
+
+        Args:
+        image (numpy.array BGR): image to preprocess
+
+        Returns:
+        input_image (numpy.array): array containing the image with the dimensions [1x3x384x672]
+        '''
+        # Get the input shape of the model
+        net_input_shape = self.get_input_shape()
+
+        # Resize the image to the input of the model
+        input_image = cv2.resize(image, (net_input_shape[3], net_input_shape[2]))
+
+        # Get the Color Channels in front
+        input_image = input_image.transpose((2,0,1))
+
+        # Add one dimension at the front for the batch size
+        input_image = input_image.reshape(1, *input_image.shape)
+
+        return input_image
+
+    def postprocess_output(self, output):
+        '''
+        Postprocss the output of the model.
+        Needs to be defined for each model.
+        '''
+        raise NotImplementedError
+    
+    def predict(self, image):
+        '''
+        Do the full prediction on an image.
+        Needs to be defined for each model.
+        '''
         raise NotImplementedError
 
-    def preprocess_output(self, outputs):
-    '''
-    Before feeding the output of this model to the next model,
-    you might have to preprocess the output. This function is where you can do that.
-    '''
-        raise NotImplementedError
