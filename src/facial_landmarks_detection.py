36192762958f39ffeb00eda3547be1fa7308189b
Lucas Vandroux <lucas.vandroux@gmail.com>
2020-06-20 01:30:43 +0200

    Add function to display the landmark on the image.

--- src/facial_landmarks_detection.py
+++ src/facial_landmarks_detection.py
@@ -1,5 +1,7 @@
 from model import OpenVINOModel
 from utils import Landmark
+
+import cv2
 '''
 Documentation: https://docs.openvinotoolkit.org/latest/_models_intel_landmarks_regression_retail_0009_description_landmarks_regression_retail_0009.html
 
@@ -7,7 +9,7 @@ Download: python /opt/intel/openvino/deployment_tools/open_model_zoo/tools/downl
 '''
 
 
-class HeadPoseEstimationModel(OpenVINOModel):
+class FacialLandmarksDetectionModel(OpenVINOModel):
     '''
     Class for the Head Pose Estimation Model.
     '''
@@ -28,7 +30,7 @@ class HeadPoseEstimationModel(OpenVINOModel):
         output = self.inference(input_image)
         
         # Postprocess the output of the model
-        results = self.postprocess_output(output, image)
+        results = self.postprocess_output(output[self.output_blob], image)
 
         return results
 
@@ -37,7 +39,7 @@ class HeadPoseEstimationModel(OpenVINOModel):
         Postprocss the output of the model
 
         Args:
-            output (numpy.array): array [1, 10] containing the landmarks' position
+            output (numpy.array): array [1, 10, 1, 1] containing the landmarks' position
             image (numpy.array): original image used for the inference
 
         Returns:
@@ -60,10 +62,49 @@ class HeadPoseEstimationModel(OpenVINOModel):
             name = list_landmarks_name[idx]
 
             # Extract position of the landmark and unormalized them
-            x = output[0,idx*2] * image_w
-            y = output[0,idx*2+1] * image_h
+            x = int(output[0,idx*2,0,0] * image_w)
+            y = int(output[0,idx*2+1,0,0] * image_h)
 
             # Add the landmark to the list of detected landmarks
             list_landmarks.append(Landmark(name, x, y))
 
         return list_landmarks
+
+    def display_output(self, image, results, color = (0, 0, 255), display_name = False):
+        '''
+        Display the bounding boxes on the image.
+
+        Args:
+            image (numpy.array): original image used for the inference.
+            results (list[Landmark]): list of the facial landmarks
+            color ((B, G, R): Red): color to draw the bounding boxes
+            display_name (Bool: False): display the name of the landmarks
+
+        Returns:
+            image_out (numpy.array): copy of the input image with the bounding 
+                boxes of the detected head
+        '''
+        # Copy the input image
+        image_out = image.copy()
+
+        for landmark in results:
+            # Mark the landmark on the face
+            cv2.circle(
+                image_out, 
+                (landmark.x, landmark.y), 
+                2, 
+                color, 
+                -1)
+
+            if display_name:
+                # Write the name of the landmark
+                cv2.putText(
+                    image_out,
+                    landmark.n,
+                    (landmark.x, landmark.y - 5),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.4 ,
+                    color
+                )
+        
+        return image_out
